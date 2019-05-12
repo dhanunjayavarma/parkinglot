b25186ea20f2b93432a05af8b05baf94b71fb68f
dhanunjayavarma <dhanunjayavarmak@gmail.com>
2019-05-12 14:13:32 +0530

    changed constructor for ParkingLot

--- test/ParkingLotTest.java
+++ test/ParkingLotTest.java
@@ -2,44 +2,40 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+
 import static org.mockito.Mockito.*;
+
 public class ParkingLotTest {
     AutomatedTicketSystem ticketSystem;
     ParkingLot parkingLot;
 
     @Before
-    public void SetUp(){
-        parkingLot=ParkingLot.getInstance(3);
-        ticketSystem=mock(AutomatedTicketSystem.class);
-        parkingLot.ticketSystem=ticketSystem;
+    public void SetUp() {
+        ticketSystem = mock(AutomatedTicketSystem.class);
+        parkingLot = ParkingLot.getInstance(ticketSystem,3);
+        parkingLot.ticketSystem = ticketSystem;
     }
 
     @Test
-    public void TestFor_Park(){
+    public void TestPark() {
         parkingLot.park();
-        verify(ticketSystem,times(1)).allocateParkingSlot();
+        verify(ticketSystem, times(1)).allocateParkingSlot();
     }
 
     @Test
-    public void TestFor_IsParkingLotFull(){
-        boolean parkingLotFull=parkingLot.isParkingLotFull();
-        Assert.assertEquals(false,parkingLotFull);
+    public void TestForIsParkingLotFull() {
+        boolean parkingLotFull = parkingLot.isParkingLotFull();
+        Assert.assertEquals(false, parkingLotFull);
     }
 
     @Test
-    public void TestForIsParkingLotFull_When_ParkingLot_IS_Full(){
+    public void TestForIsParkingLotFull_When_ParkingLot_IS_Full() {
 
     }
 
     @Test
-    public void TestFor_UnPark(){
+    public void TestForUnPark() {
         parkingLot.unPark();
-        verify(ticketSystem,times(1)).freeParkingSlot();
-    }
-
-    @Test
-    public void TestFor_GetNearestParkingSlotNumberToTheEntry(){
-        int parkingSlotNumber=parkingLot.getNearestParkingSlotNumberToTheEntry();
-        Assert.assertEquals(1,parkingSlotNumber);
+        verify(ticketSystem, times(1)).freeParkingSlot();
     }
 }
