200d609599ba1c3a32baca38f8e9f315f745da68
dhanunjayavarma <dhanunjayavarmak@gmail.com>
2019-05-12 08:29:37 +0530

    Added Test for park method which will be in ParkingLot class

--- test/ParkingLotTest.java
+++ test/ParkingLotTest.java
@@ -1,11 +1,18 @@
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ParkingLotTest {
     ParkingLot parkingLot;
+
     @Before
     public void SetUp(){
         parkingLot=new ParkingLot();
+    }
+
+    @Test
+    public void TestPark(){
+        System.out.println("Method Fails");
     }
 }
