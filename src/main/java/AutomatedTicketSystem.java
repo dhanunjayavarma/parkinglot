231cd94c5ff4295064affe0cd0ef15f8acb8dde8
dhanunjayavarma <dhanunjayavarmak@gmail.com>
2019-05-12 20:28:24 +0530

    added methods to display the asked data

--- src/main/java/AutomatedTicketSystem.java
+++ src/main/java/AutomatedTicketSystem.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
+import java.util.List;
 import java.util.Map;
 
 public class AutomatedTicketSystem {
@@ -32,5 +34,65 @@ public class AutomatedTicketSystem {
     public void collectTicket(int parkingSlotNumber) {
         parkingLotDetalis.remove(parkingSlotNumber);
         parkingLot.freeParkingSlot(parkingSlotNumber);
+    }
+
+    public void showRegistrationNumberOfCarsWithGivenColour(Colour colour) {
+        List<String> registrationNumberList = getRegistrationNumberOfCarsWithGivenColour(colour);
+
+        showList(registrationNumberList);
+    }
+
+    private List<String> getRegistrationNumberOfCarsWithGivenColour(Colour colour) {
+        List<String> registrationNumbersList = new ArrayList<String>();
+
+        for (Map.Entry<Integer, ParkingLotDetalisVO> entry : parkingLotDetalis.entrySet()) {
+            if (entry.getValue().getColour().equals(colour)) {
+                registrationNumbersList.add(entry.getValue().getRegistrationNumber());
+            }
+        }
+        return registrationNumbersList;
+    }
+
+    public void showSlotNumbersOfCarsWithGivenColour(Colour colour) {
+        List<Integer> slotNumberList = getSlotNumberOfCarsWithGivenColour(colour);
+        showList(slotNumberList);
+    }
+
+    private List<Integer> getSlotNumberOfCarsWithGivenColour(Colour colour) {
+        List<Integer> slotNumbersList = new ArrayList<Integer>();
+
+        for (Map.Entry<Integer, ParkingLotDetalisVO> entry : parkingLotDetalis.entrySet()) {
+            if (entry.getValue().getColour().equals(colour)) {
+                slotNumbersList.add(entry.getValue().getSlotNumber());
+            }
+        }
+        return slotNumbersList;
+    }
+
+    public void showSlotNumbersOfCarsWithGivenRegistrationNumber(String registrationNumber) {
+        List<Integer> slotNumberList = getSlotNumberOfCarsWithGivenRegistrationNumber(registrationNumber);
+        showList(slotNumberList);
+    }
+
+    private List<Integer> getSlotNumberOfCarsWithGivenRegistrationNumber(String registrationNumber) {
+        List<Integer> slotNumbersList;
+        slotNumbersList = new ArrayList<Integer>();
+
+        for (Map.Entry<Integer, ParkingLotDetalisVO> entry : parkingLotDetalis.entrySet()) {
+            if (entry.getValue().getColour().equals(registrationNumber)) {
+                slotNumbersList.add(entry.getValue().getSlotNumber());
+            }
+        }
+        return slotNumbersList;
+    }
+
+    private void showList(List list) {
+        for (int index = 0; index < list.size(); index++) {
+            if (index == list.size() - 1) {
+                System.out.println(list.get(index));
+            } else {
+                System.out.print(list.get(index) + ", ");
+            }
+        }
     }
 }
