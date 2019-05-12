4bd1a0c2914191dc2f784609c5b3257704f5fd93
dhanunjayavarma <dhanunjayavarmak@gmail.com>
2019-05-12 18:29:43 +0530

    Revert "implemented allocateTicket Method and now the TEST IS PASS"
    This reverts commit b1b9cbee

--- src/main/java/AutomatedTicketSystem.java
+++ src/main/java/AutomatedTicketSystem.java
@@ -1,27 +1,10 @@
 public class AutomatedTicketSystem {
-    ParkingLot parkingLot;
 
-    public ParkingLot getParkingLot() {
-        return parkingLot;
+    public void allocateParkingSlot() {
+        System.out.println("hello");
     }
 
-    public void setParkingLot(ParkingLot parkingLot) {
-        this.parkingLot = parkingLot;
-    }
-
-    public void allocateTicket(Car car) {
-        final int availableParkingSlotNumber = parkingLot.getNearestParkingSlotNumberToTheEntry();
-        String ticket = genarateTicket(availableParkingSlotNumber, car.getRegistrationNumber());
-        car.setTicket(ticket);
-        parkingLot.allocateParkingSlot(availableParkingSlotNumber, car);
-    }
-
-    private String genarateTicket(int slotNumber, String registrationNumber) {
-        final String ticket = slotNumber + registrationNumber;
-        return ticket;
-    }
-
-    public void collectTicket(int parkingSlotNumber) {
+    public void freeParkingSlot() {
 
     }
 }
