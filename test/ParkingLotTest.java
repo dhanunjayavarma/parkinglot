18b854f1faa4ad0eaf1f684e8d4f142488ceedc2
dhanunjayavarma <dhanunjayavarmak@gmail.com>
2019-05-12 08:24:11 +0530

    Added SetUp Method

--- test/ParkingLotTest.java
+++ test/ParkingLotTest.java
@@ -1,5 +1,11 @@
+import org.junit.Before;
+
 import static org.junit.Assert.*;
 
 public class ParkingLotTest {
-
+    ParkingLot parkingLot;
+    @Before
+    public void SetUp(){
+        parkingLot=new ParkingLot();
+    }
 }
